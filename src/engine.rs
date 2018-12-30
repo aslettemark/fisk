5b607c35bd80f5ab1b034ad88d99aeef3ea74332
Aksel Slettemark <akselslettemark@gmail.com>
2018-12-30 02:56:50 +0100

    Finish default chess board.

--- src/engine.rs
+++ src/engine.rs
@@ -26,7 +26,11 @@ const WHITE_QUEEN: u8 = 1 << 4;
 const WHITE_KING: u8 = 1 << 5;
 
 const BLACK_PAWN: u8 = WHITE_PAWN | (1 << 7);
-//TODO
+const BLACK_BISHOP: u8 = WHITE_BISHOP | (1 << 7);
+const BLACK_KNIGHT: u8 = WHITE_KNIGHT | (1 << 7);
+const BLACK_ROOK: u8 = WHITE_ROOK | (1 << 7);
+const BLACK_QUEEN: u8 = WHITE_QUEEN | (1 << 7);
+const BLACK_KING: u8 = WHITE_KING | (1 << 7);
 
 pub struct Board {
     pub halfturn: u16,
@@ -43,7 +47,16 @@ struct BitBoard {
     pub white_pawns: u64,
     pub white_queen: u64,
     pub white_king: u64,
-    //TODO
+    pub white_rooks: u64,
+    pub white_bishops: u64,
+    pub white_knights: u64,
+
+    pub black_pawns: u64,
+    pub black_queen: u64,
+    pub black_king: u64,
+    pub black_rooks: u64,
+    pub black_bishops: u64,
+    pub black_knights: u64,
 }
 
 struct Piece {
@@ -51,10 +64,43 @@ struct Piece {
     pub position: u64,
 }
 
+impl Piece {
+    pub fn new(kind: u8, position: u64) -> Piece {
+        Piece {
+            kind: kind,
+            position: position,
+        }
+    }
+}
+
 impl Board {
     // Create default chess board
     pub fn new() -> Board {
-        let ps = vec![Piece { kind: WHITE_PAWN, position: ROW_2 & FILE_A }];
+        let mut ps: Vec<Piece> = Vec::with_capacity(32);
+
+        for i in 0..8 {
+            ps.push(Piece::new(WHITE_PAWN, ROW_2 & (FILE_A << i)));
+            ps.push(Piece::new(BLACK_PAWN, ROW_7 & (FILE_A << i)));
+        }
+
+        ps.push(Piece::new(WHITE_KING, ROW_1 & FILE_E));
+        ps.push(Piece::new(WHITE_QUEEN, ROW_1 & FILE_D));
+        ps.push(Piece::new(WHITE_BISHOP, ROW_1 & FILE_C));
+        ps.push(Piece::new(WHITE_BISHOP, ROW_1 & FILE_F));
+        ps.push(Piece::new(WHITE_KNIGHT, ROW_1 & FILE_B));
+        ps.push(Piece::new(WHITE_KNIGHT, ROW_1 & FILE_G));
+        ps.push(Piece::new(WHITE_ROOK, ROW_1 & FILE_A));
+        ps.push(Piece::new(WHITE_ROOK, ROW_1 & FILE_H));
+
+        ps.push(Piece::new(BLACK_KING, ROW_8 & FILE_E));
+        ps.push(Piece::new(BLACK_QUEEN, ROW_8 & FILE_D));
+        ps.push(Piece::new(BLACK_BISHOP, ROW_8 & FILE_C));
+        ps.push(Piece::new(BLACK_BISHOP, ROW_8 & FILE_F));
+        ps.push(Piece::new(BLACK_KNIGHT, ROW_8 & FILE_B));
+        ps.push(Piece::new(BLACK_KNIGHT, ROW_8 & FILE_G));
+        ps.push(Piece::new(BLACK_ROOK, ROW_8 & FILE_A));
+        ps.push(Piece::new(BLACK_ROOK, ROW_8 & FILE_H));
+
         Board {
             halfturn: 0,
             en_passant: 0,
@@ -62,8 +108,17 @@ impl Board {
                 white_pawns: ROW_2,
                 white_queen: ROW_1 & FILE_D,
                 white_king: ROW_1 & FILE_E,
+                white_rooks: ROW_1 & (FILE_A | FILE_H),
+                white_bishops: ROW_1 & (FILE_C | FILE_F),
+                white_knights: ROW_1 & (FILE_B | FILE_G),
+                black_pawns: ROW_7,
+                black_queen: ROW_8 & FILE_D,
+                black_king: ROW_8 & FILE_E,
+                black_rooks: ROW_8 & (FILE_A | FILE_H),
+                black_bishops: ROW_8 & (FILE_C | FILE_F),
+                black_knights: ROW_8 & (FILE_B | FILE_G),
             },
-            pieces: ps, //TODO
+            pieces: ps,
             castling: 0b11101110,
         }
     }
@@ -88,11 +143,9 @@ impl Board {
 
         println!(" A B C D E F G H");
 
-
-
-        for p in self.pieces.iter() {
-            println!("{:#064b}: {}", p.position, self.piece_representation(p.kind));
-        }
+        /*for p in self.pieces.iter() {
+            println!("{:#066b}: {}", p.position, self.piece_representation(p.kind));
+        }*/
     }
 
     fn piece_representation(&self, kind: u8) -> char {
@@ -104,7 +157,11 @@ impl Board {
             WHITE_QUEEN => '♕',
             WHITE_KING => '♔',
             BLACK_PAWN => '♟',
-            //TODO
+            BLACK_BISHOP => '♝',
+            BLACK_KNIGHT => '♞',
+            BLACK_ROOK => '♜',
+            BLACK_QUEEN => '♛',
+            BLACK_KING => '♚',
             _ => ' '
         }
     }
@@ -120,7 +177,34 @@ impl Board {
         if pos & b.white_king != 0 {
             return WHITE_KING;
         }
-        //TODO
+        if pos & b.white_bishops != 0 {
+            return WHITE_BISHOP;
+        }
+        if pos & b.white_knights != 0 {
+            return WHITE_KNIGHT;
+        }
+        if pos & b.white_rooks != 0 {
+            return WHITE_ROOK;
+        }
+
+        if pos & b.black_pawns != 0 {
+            return BLACK_PAWN;
+        }
+        if pos & b.black_queen != 0 {
+            return BLACK_QUEEN;
+        }
+        if pos & b.black_king != 0 {
+            return BLACK_KING;
+        }
+        if pos & b.black_bishops != 0 {
+            return BLACK_BISHOP;
+        }
+        if pos & b.black_knights != 0 {
+            return BLACK_KNIGHT;
+        }
+        if pos & b.black_rooks != 0 {
+            return BLACK_ROOK;
+        }
 
         return EMPTY_SQUARE;
     }
